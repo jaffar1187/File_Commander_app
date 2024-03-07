6489e8606eab3a07fc680942a43e1ca465e238df
Jaffar Sharieff <jaffar1187@gmail.com>
2024-03-07 15:11:14 +0530

    added rest functionalities

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require("fs/promises");
 const command_file = "./command.txt";
 const CREATE_FILE = "create a file";
+const DELETE_FILE = "delete a file";
+const RENAME_FILE = "rename a file";
+const ADD_TO_FILE = "add to the file";
+
 let commandFileHandler;
 
 const start = async () => {
@@ -17,20 +21,6 @@ const start = async () => {
     //Checking if file is change
     if (event.eventType === "change" && event.filename === "command.txt")
       commandFileHandler.emit("change");
-  }
-};
-
-const createFile = async (path) => {
-  try {
-    // Checking if file already exists, In case it does not exists we will get an error.
-    const existingFileHandle = await fs.open(path, "r");
-    existingFileHandle.close();
-    return console.log(`The file ${path} already exists.`);
-  } catch (e) {
-    //Create a new file if it does not exists.
-    const newFile = await fs.open(path, "w");
-    console.log("A new file was successfully created.");
-    newFile.close();
   }
 };
 
@@ -56,6 +46,82 @@ const changesHandler = async () => {
   if (command.includes(CREATE_FILE)) {
     const filePath = command.substring(CREATE_FILE.length + 1);
     await createFile(filePath);
+  }
+
+  // 2> Delete a file
+  else if (command.includes(DELETE_FILE)) {
+    const filePath = command.substring(DELETE_FILE.length + 1);
+    await deleteFile(filePath);
+  }
+
+  // 3> Rename a file
+  else if (command.includes(RENAME_FILE)) {
+    const filePath = command.substring(RENAME_FILE.length + 1);
+    const old_file_path = filePath.split(" ")[0];
+    const new_file_path = filePath.split(" ")[1];
+    await renameFile(old_file_path, new_file_path);
+  }
+
+  // 4> Add to the file
+  else if (command.includes(ADD_TO_FILE)) {
+    const filePath = command.substring(ADD_TO_FILE.length + 1);
+    await addTofile(filePath, "This is just some text");
+  }
+};
+
+const fileExistsCheck = async (path) => {
+  const existingFileHandle = await fs.open(path, "r");
+  await existingFileHandle.close();
+};
+
+//File modification functions.
+const createFile = async (path) => {
+  try {
+    // Checking if file already exists, In case it does not exists we will get an error.
+    await fileExistsCheck(path);
+    return console.log(`The file ${path} already exists.`);
+  } catch (e) {
+    //Create a new file if it does not exists.
+    const newFile = await fs.open(path, "w");
+    console.log("A new file was successfully created.");
+    newFile.close();
+  }
+};
+
+const deleteFile = async (path) => {
+  try {
+    // Checking if file already exists, In case it does not exists we will get an error.
+    await fileExistsCheck(path);
+    console.log("Deleting file...");
+    await fs.unlink(path);
+    return console.log(`The file ${path} is been successfully deleted.`);
+  } catch (e) {
+    console.log("The file does not exists.");
+  }
+};
+
+const addTofile = async (path, data) => {
+  try {
+    // Checking if file already exists, In case it does not exists we will get an error.
+    const existingFileHandle = await fs.appendFile(path, data);
+    await existingFileHandle.close();
+    return console.log(`Added data to file successfully.`);
+  } catch (e) {
+    console.log("Some error occurred while adding to file.");
+  }
+};
+
+const renameFile = async (old_file_path, new_file_path) => {
+  try {
+    // Checking if file already exists, In case it does not exists we will get an error.
+    console.log(old_file_path, new_file_path);
+    const existingFileHandle = await fs.open(old_file_path, "r");
+    await existingFileHandle.close();
+    console.log("Renaming file...");
+    await fs.rename(old_file_path, new_file_path);
+    return console.log(`The file is been successfully renamed.`);
+  } catch (e) {
+    console.log("The file does not exists.");
   }
 };
 
